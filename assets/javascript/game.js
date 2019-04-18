13fd57f8b98d2e1bc200bf073162daee7a244f0f
cclivick <46388110+cclivick@users.noreply.github.com>
2019-04-18 12:23:10 -0500

    Day 2 Progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,58 @@
 
-var numGuesses = 
+// Creates an array of possible words for hangman game //
+
+var horrorIcons = [
+    "jason",
+    "freddy",
+    "pennywise",
+    "leatherface",
+    "chucky",
+    "jaws",
+    "elvira",
+    "michaelmyers",
+    "pinhead",
+    "normanbates",
+    "candyman",
+    "hannibal",
+    "jigsaw",
+    "ghostface",
+    "dracula",
+    "thething",
+    "alien",
+    "predator",
+    "tallman",
+    "poltergeist"
+]
+
+
+// Randomly selects string from var horrorIcons //
+
+var icon = horrorIcons[Math.floor(Math.random() * horrorIcons.length)];
+console.log(icon)
+
+var underscore = [];
+
+// Creates underscores based on length of word //
+
+var makeUnderscore = () => {
+    for(var i=0; i<icon.length; i++) {
+        underscore.push("_");
+    }return underscore;
+}
+
+console.log(makeUnderscore());
+
+// Record keys user presses //
+
+document.onkeyup = function() {
+    var userGuess = String.fromCharCode(event.keyCode).
+    toLowerCase();
+
+    console.log(userGuess)
+}
+
+// Determines if userGuess is found in word //
+
+// If found in word, push to right array //
+
+// if not found in word, push to wrong array //
